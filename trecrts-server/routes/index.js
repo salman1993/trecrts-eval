5c3c5387e456962bc0a1828f823a4512b8bc3a81
Salman Mohammed <salman93@live.com>
2017-06-12 20:45:13 -0400

    working links

--- trecrts-server/routes/index.js
+++ trecrts-server/routes/index.js
@@ -153,7 +153,8 @@ module.exports = function(io){
   var rel2id = {"notrel": 0, "rel": 1, "dup": 2}
 
   function generate_judgement_link(topid, tweetid, relid, partid) {
-    var hostname = "http://ts4.io";
+    //var hostname = "http://ts4.io";
+    var hostname = "http://scspc654.cs.uwaterloo.ca";
     // var port = 10101;
     // var link = util.format('%s:%s/judge/%s/%s/%s/%s', hostname, port, topid, tweetid, relid, partid);
     var link = util.format('%s/judge/%s/%s/%s/%s', hostname, topid, tweetid, relid, partid);
