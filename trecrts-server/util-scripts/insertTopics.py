7b16cfd6c1a99df1a69c1b1ae058f61de492c6b7
Salman Mohammed <salman93@live.com>
2017-07-13 22:12:29 -0400

    modified insert topics script

--- trecrts-server/util-scripts/insertTopics.py
+++ trecrts-server/util-scripts/insertTopics.py
@@ -3,7 +3,7 @@ import json
 import MySQLdb
 
 
-db = MySQLdb.connect('localhost','','','trec_rts')
+db = MySQLdb.connect('localhost','salman','','trec_rts')
 cursor = db.cursor()
 
 topicsfil = open(sys.argv[1])
@@ -13,13 +13,13 @@ for topic in topiclist:
   title=topic["title"]
   desc=topic["description"]
   narr=topic["narrative"]
-#  print('insert into topics (topid,title,description,narrative) values ("{0}","{1}","{2}","{3}");'.format(topid,title,desc,narr));
-#  print('create table seen_{0}  like seen_templare;'.format(topid));
-#  print('create table judgements_{0}  like judgements_templare;'.format(topid));
+  #print('insert into topics (topid,title,description,narrative) values ("{0}","{1}","{2}","{3}");'.format(topid,title,desc,narr));
+  #print('create table seen_{0}  like seen_templare;'.format(topid));
+  #print('create table judgements_{0}  like judgements_templare;'.format(topid));
   print("Doing: ",topid)
   cursor.execute("""insert into topics (topid,title,description,narrative) values (%s,%s,%s,%s);""",(topid,title,desc,narr));
-  cursor.execute('create table if not exists seen_{0}  like seen_template;'.format(topid));
-  cursor.execute('create table if not exists judgements_{0}  like judgements_template;'.format(topid));
+  #cursor.execute('create table if not exists seen_{0}  like seen_template;'.format(topid));
+  #cursor.execute('create table if not exists judgements_{0}  like judgements_template;'.format(topid));
 
 topicsfil.close()
 db.commit()
